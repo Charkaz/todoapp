4a4a008c3ad471803eefbe11443a0a1c5f4fb835
Alshan <39612986+Charkaz@users.noreply.github.com>
2022-07-18 14:52:54 +0400

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -32,7 +32,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'todoapp98@gmail.com'
-EMAIL_HOST_PASSWORD = '123456789todo'
+EMAIL_HOST_PASSWORD = '*********'
 
 
 # Application definition
